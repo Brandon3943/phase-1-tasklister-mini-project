a20c32e566cc9619b80f2e33abd5643da7528796
root <root@Ty-Laptop.localdomain>
2022-11-19 14:43:03 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    handleToDo(e.target["new-task-description"].value)
+    form.reset();
+  })
 });
+
+function handleToDo(toDo) {
+  const p = document.createElement('p');
+  const btn = document.createElement('button');
+  btn.textContent = 'X';
+  btn.addEventListener('click', handleDelete)
+  p.textContent = `${toDo}  `;
+  p.appendChild(btn);
+  document.querySelector("#list").appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
